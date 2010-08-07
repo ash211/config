c79220b0933608c34fbe1dcd15573ed639120a6b
Andrew Ash <ash211@gmail.com>
2010-08-07 14:08:27 -0400

    Use the exceedingly handy install command instead of my hacked-together logic

--- install.sh
+++ install.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-if ( [ ! -f ~/.vimrc ] || ( [ -f ~/.vimrc ] && [ ! -f ~/.vimrc.old ] ) )
-then
-    if [ -f ~/.vimrc ]
-    then
-        echo "Backing up ~/.vimrc to ~/.vimrc.old"
-        mv ~/.vimrc ~/.vimrc.old
-    fi
-    echo "Installing new .vimrc"
-    cp vimrc ~/.vimrc
-else
-    if [ -f ~/.vimrc.old ]
-    then
-        echo "Not updating .vimrc ... detected ~/.vimrc.old"
-    fi
-fi
+echo "Installing... vimrc"
+install --mode=644 --backup=t vimrc ~/.vimrc
+echo "done."
+exit
