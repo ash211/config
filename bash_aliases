305c912acc7c90424cc6ab992da4d108e40c4210
Andrew Ash <ash211@gmail.com>
2010-08-07 15:18:37 -0400

    Add bash_aliases

---
+++ bash_aliases
@@ -0,0 +1,3 @@
+alias la='ls -a'
+alias ll='ls -l'
+alias lld='ls -ld'
--- install.sh
+++ install.sh
@@ -10,5 +10,8 @@ pushd vim
 find . -type f -exec install -D {} ~/.vim/{} \;
 popd
 
+echo "Installing... ~/.bash_aliases"
+install --mode=644 --backup=t -D bash_aliases ~/.bash_aliases
+
 echo "done."
 exit
